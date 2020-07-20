9533e9c1a18e46ae1b26f8664cec78f70c6264b9
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-20 23:16:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,45 @@
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+  ["red", "blue", "yellow", "pink"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array
+    ["red", "blue", "yellow", "pink"]
+    add_element_to_end_of_array.push"white"
+
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array
+    []
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
+
 end
 
 def update_element_from_index(array, index_number, element)
+
 
 end
